735733a8f9812089c2f43bbbf85b847ce7823f56
aMir <4mir.733@gmail.com>
2022-11-23 00:40:57 +0330

    Better file structure

--- db.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import SQLModel, create_engine
-
-class Database(object):
-    def __init__(self, path):
-        Database.engine = create_engine(f"sqlite://{path}")
-    def create(self):
-        SQLModel.metadata.create_all(engine)
